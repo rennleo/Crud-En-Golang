fd77288840042d1d7d1ca90d1eefc8bf9a579c9b
rennleo <rennleonardo@gmail.com>
2023-03-29 12:21:12 -0300

    29/03

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main(){
 	http.HandleFunc("/", Inicio)
+	http.HandleFunc("/crear", Crear)
 
 	log.Println("Servidor corrriendo")
 	http.ListenAndServe(":8080",nil)
@@ -17,5 +18,10 @@ func main(){
 func Inicio(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w,"hola develoteca")
 	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+func Crear(w http.ResponseWriter, r *http.Request) {
+	plantillas.ExecuteTemplate(w,"crear",nil)
+	
 }
 
